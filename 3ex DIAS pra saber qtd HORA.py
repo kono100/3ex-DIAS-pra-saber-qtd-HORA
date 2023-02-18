cba081f06b04a9936208fa88f7e6605cd728c30c
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-18 17:22:53 -0300

    Add files via upload

---
+++ 3ex DIAS pra saber qtd HORA.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+#3. Faça um programa que dado uma quantidade “X” de dias, calcule e exiba a quantidade
+#equivalente em horas.
+
+Dias = int(input("Digite quantos dias, pra saber quantas horas tem : "))
+DiasH = 24*Dias
+
+print(f"{Dias} Dias Quantidade de :{DiasH}Hrs")
